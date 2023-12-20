581c0f5c639b8514831d6accadeb5069b8a3c9c6
Hanxiao Chai <74058403+HXC-A@users.noreply.github.com>
2023-12-20 03:01:25 -0500

    middleware

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+import logging
 
 db = SQLAlchemy()
 
@@ -12,5 +13,42 @@ def create_app():
 
     from .routes.user_routes import user_blueprint
     app.register_blueprint(user_blueprint)
+
+    logging.basicConfig( level=logging.INFO)
+
+
+    file_handler = logging.FileHandler('mdwarerecordinglog.log')
+    file_handler.setLevel(logging.INFO)
+
+
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    file_handler.setFormatter(formatter)
+
+
+    logger = logging.getLogger(__name__)
+    logger.addHandler(file_handler)
+
+    # Custom middleware of recording log
+    class LoggingMiddleware:
+        def __init__(self, app):
+            self.app = app
+
+        def __call__(self, environ, start_response):
+
+            print(start_response)
+            logger.info('Handling request: %s %s', environ['REQUEST_METHOD'], environ['PATH_INFO'])
+
+            def custom_start_response(status, headers, exc_info=None):
+                
+                print(status)
+                logger.info('Response returned: %s', status)
+
+                #
+                return start_response(status, headers, exc_info)
+
+            # Use flask to deal with rueqests
+            return self.app(environ, custom_start_response)
+
+    app.wsgi_app = LoggingMiddleware(app.wsgi_app)
 
     return app
