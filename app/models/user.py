2d3d43463146f02700a111d282a0ef899307069c
Xiyue Wang <xiyuewang@Xiyues-MacBook-Pro.local>
2023-12-21 21:11:50 -0500

    Test CI/CD pipeline

--- app/models/user.py
+++ app/models/user.py
@@ -4,4 +4,4 @@ class User(db.Model):
     userid = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), nullable=False)
     email = db.Column(db.String(100), nullable=False, unique=True)
-    profile_picture = db.Column(db.String(300))  # Assuming you store image URLs or paths
+    profile_picture = db.Column(db.String(250))  # Assuming you store image URLs or paths
